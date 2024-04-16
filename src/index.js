5fb004881ce257f262bb16a925549e5117fb8b26
dipak-kumawat <dipaksk11@gmail.com>
2024-04-16 09:05:23 +0530

    adding error page in App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import Header from "./header";
 import Dish from "./dish";
 import About from "./about.js";
+import Error from "./error.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 const Page = () => (
   <>
@@ -12,6 +15,18 @@ const Page = () => (
   </>
 );
 
+const appRounter = createBrowserRouter([
+  {
+    path:"/",
+    element: <Page/>,
+    errorElement: <Error/>
+  },
+  {
+    path: "/about",
+    element: <About/>
+  }]
+)
+
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Page/>);
+root.render(<RouterProvider router={appRounter}/>);
